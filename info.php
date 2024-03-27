9828ed3805f6dea95bf217f2cf26e4fe89ba6ebd
ForsanNemah <forsan20172017@gmail.com>
2024-03-27 06:59:50 +0300

    s

--- info.php
+++ info.php
@@ -3,7 +3,7 @@
 
 $web_site_name="مجمع عيادات د جميل خطاب ";
 $email="info@jameelkhattab.com";
-$phone="9966568430828";
+$phone="966559833446";
 $address="Al Hufuf and Al Mubarraz 5046 الهفوف والمبرز";
 
 
