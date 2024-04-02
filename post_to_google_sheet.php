c4fcc29ca77f132d529ea6db7d6c5189aac72ff0
ForsanNemah <forsan20172017@gmail.com>
2024-04-02 17:33:18 +0300

    hh

--- post_to_google_sheet.php
+++ post_to_google_sheet.php
@@ -37,12 +37,23 @@ $postParameter = array(
     
 );
 
+
+
 $curlHandle = curl_init($action_url);
 curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $postParameter);
 curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, false);
 $curlResponse = curl_exec($curlHandle);
 curl_close($curlHandle);
+
+
+//echo "w_api start";
+
+$w_app_msg1=" لديك تسجيل  جديد عبر الموقع الالكتروني باسم  ".$name;
+$w_app_msg2=$w_app_msg1." برقم جوال ".$phone;
+
+send_w_app_msg("120363212806523669",$w_app_msg2,"2000");
+
 
 
 //echo $curlResponse."";
@@ -106,7 +117,47 @@ window.location.replace("index.php");
 
 
 
+function send_w_app_msg($phone,$msg,$token) {
 
+
+    
+
+//echo "w_api start 2";
+
+
+
+//echo $phone.$msg.$token;
+
+
+
+/*
+    $postParameter = array(
+        
+        
+        'phn' => $phone,
+        'token' => $token,
+        'msg' => $msg
+        
+    );
+
+    */
+
+
+    $postParameter="phn=".$phone."&msg=".$msg."&token=".$token;
+    
+    $curlHandle = curl_init("http://185.182.186.101:2000/send-text-group");
+    curl_setopt($curlHandle, CURLOPT_POST, true);
+    curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+    curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $postParameter);
+    curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, false);
+    $curlResponse = curl_exec($curlHandle);
+    echo $curlResponse;
+    curl_close($curlHandle);
+
+
+    
+  }
 
 
 
