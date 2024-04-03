2f017ebd5a90168eeec333fa59d7638c5281a3d1
ForsanNemah <forsan20172017@gmail.com>
2024-04-03 11:52:44 +0300

    sss

--- post_to_google_sheet.php
+++ post_to_google_sheet.php
@@ -159,7 +159,7 @@ echo "w_api start 2";
     //print_r(curl_getinfo($curlHandle));
 
     if(curl_errno($curlHandle)){
-        echo 'Curl error: ' . curl_error($ch);
+        echo 'Curl error: ' . curl_error($curlHandle);
     }
     
   }
